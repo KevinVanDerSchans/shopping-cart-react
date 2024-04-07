09f256918c0423667941646aab7b101d729e3211
Kevin Schans <kevinvdsd@hotmail.com>
2024-04-07 16:36:22 +0200

    Create useFilters hook

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import { Filters } from '../Filters/Filters'
 
-export function Header({ changeFilters }) {
+export function Header() {
   return (
     <header>
       <h1>Shopping ReactCart</h1>
 
-      <Filters onChange={changeFilters} />
+      <Filters />
     </header>
   )
 }
--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,43 +1,23 @@
-import { useContext, useState } from 'react'
+import { useState } from 'react'
+import { useFilters } from '../../hooks/useFilters'
 import { Header } from '../Header/Header'
 import { products as initialProducts } from '../../mocks/products.json'
 import { Products } from '../Products/Products'
 import { Footer } from '../Footer/Footer'
 import { IS_DEVELOPMENT } from '../../config'
 import './App.css'
-import { FiltersContext } from '../../context/filters'
-
-function useFilters() {
-  // const [filters, setFilters] = useState({
-  //   category: 'all',
-  //   minPrice: 0,
-  // })
-
-  const { filters, setFilters } = useContext(FiltersContext)
-
-  const filterProducts = (products) => {
-    return products.filter((product) => {
-      return (
-        product.price >= filters.minPrice &&
-        (filters.category === 'all' || product.category === filters.category)
-      )
-    })
-  }
-
-  return { filters, filterProducts, setFilters }
-}
 
 function App() {
   const [products] = useState(initialProducts)
-  const { filters, filterProducts, setFilters } = useFilters()
+  const { filterProducts } = useFilters()
 
   const filteredProducts = filterProducts(products)
 
   return (
     <>
-      <Header changeFilters={setFilters} />
+      <Header />
       <Products products={filteredProducts} />
-      {IS_DEVELOPMENT && <Footer filters={filters} />}
+      {IS_DEVELOPMENT && <Footer />}
     </>
   )
 }
