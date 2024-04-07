4d5a8c832208b709b345c48eae7db2e58ae7921d
Kevin Schans <kevinvdsd@hotmail.com>
2024-04-07 12:22:49 +0200

    Create filter functionality

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,15 +1,35 @@
-import { products } from '../../mocks/products.json'
+import { useState } from 'react'
+import { products as initialProducts } from '../../mocks/products.json'
 import { Products } from '../Products/Products'
 import './App.css'
 
 function App() {
+  const [products] = useState(initialProducts)
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minPrice: 0
+  })
+
+  const filterProducts = (products) => {
+    return products.filter(product => {
+      return (
+        product.price >= filters.minPrice &&
+        (
+          filters.category === 'all' ||
+          product.category === filters.category
+        )
+      )
+    })
+  }
+
+  const filteredProducts = filterProducts(products)
 
   return (
     <>
       <div>
         <h1>Shopping cart</h1>
-        <Products products={products}/>
 
+        <Products products={filteredProducts}/>
       </div>
     </>
   )
