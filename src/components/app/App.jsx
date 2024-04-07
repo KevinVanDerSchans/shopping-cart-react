623bd9dd1fb0eec0b863ff0155f1184716f699bd
Kevin Schans <kevinvdsd@hotmail.com>
2024-04-07 16:18:51 +0200

    Use useContext for filters

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import { Header } from '../Header/Header'
 import { products as initialProducts } from '../../mocks/products.json'
 import { Products } from '../Products/Products'
 import { Footer } from '../Footer/Footer'
 import { IS_DEVELOPMENT } from '../../config'
 import './App.css'
+import { FiltersContext } from '../../context/filters'
 
 function useFilters() {
-  const [filters, setFilters] = useState({
-    category: 'all',
-    minPrice: 0,
-  })
+  // const [filters, setFilters] = useState({
+  //   category: 'all',
+  //   minPrice: 0,
+  // })
+
+  const { filters, setFilters } = useContext(FiltersContext)
 
   const filterProducts = (products) => {
     return products.filter((product) => {
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import { FiltersProvider } from './context/filters'
 import ReactDOM from 'react-dom/client'
 import App from './components/app/App'
 import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <FiltersProvider>
     <App />
-  </React.StrictMode>
+  </FiltersProvider>
 )
